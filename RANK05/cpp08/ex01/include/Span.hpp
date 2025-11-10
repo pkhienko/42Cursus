8d6d53ae6504214008ce57ff73e3a25d781d2bba
pkhienko <pannatorn1222@gmail.com>
2025-11-11 03:56:35 +0700

    CPP08: ex00 - Initial files (cpp, hpp, Makefile) and Makefile

---
+++ RANK05/cpp08/ex01/include/Span.hpp
@@ -0,0 +1,19 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <list>
+# include <deque>
+# include <vector>
+# include <string>
+# include <iostream>
+# include <algorithm>
+# include <exception>
+
+# define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define BLUE "\033[1;34m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define MAGENTA "\033[1;35m"
+
+#endif
