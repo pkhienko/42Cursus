c834c9c570f4a5292812f799375078dd20caac92
pkhienko <pannatorn1222@gmail.com>
2025-11-12 01:37:48 +0700

    CPP08: ex02 - Initial files (cpp, hpp, tpp, Makefile)

---
+++ RANK05/cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,19 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <ctime>
+# include <vector>
+# include <limits>
+# include <cstdlib>
+# include <iostream>
+# include <algorithm>
+# include <exception>
+
+# define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define BLUE "\033[1;34m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define MAGENTA "\033[1;35m"
+
+#endif
