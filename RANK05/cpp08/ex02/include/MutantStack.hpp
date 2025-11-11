fe7e3ebfc624cea0053fa79715dade2255c906cc
pkhienko <pannatorn1222@gmail.com>
2025-11-12 03:16:32 +0700

    CPP08: ex02 - Implement MutantStack.hpp

--- RANK05/cpp08/ex02/include/MutantStack.hpp
+++ RANK05/cpp08/ex02/include/MutantStack.hpp
@@ -1,13 +1,11 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-# include <ctime>
+# include <list>
+# include <stack>
+# include <deque>
 # include <vector>
-# include <limits>
-# include <cstdlib>
 # include <iostream>
-# include <algorithm>
-# include <exception>
 
 # define RESET "\033[0m"
 # define RED "\033[1;31m"
@@ -15,5 +13,42 @@
 # define GREEN "\033[1;32m"
 # define YELLOW "\033[1;33m"
 # define MAGENTA "\033[1;35m"
+
+template <typename T, typename Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		// Constructors
+		MutantStack( void );
+		MutantStack( const MutantStack &other );
+
+		// Operators Overload
+		MutantStack	&operator=( const MutantStack &other );
+
+		// Destructor
+		~MutantStack( void );
+
+		// Typedef Iterators
+		typedef	typename Container::iterator iterator:
+		typedef	typename Container::const_iterator const_iterator:
+		typedef	typename Container::reverse_iterator reverse_iterator:
+		typedef	typename Container::const_reverse_iterator const_reverse_iterator:
+
+		 // Normal Iterators
+		iterator	begin( void );
+		iterator	end( void );
+
+		// Const Iterators
+		const_iterator	begin( void ) const;
+		const_iterator	end( void ) const;
+
+		// Reverse Iterators
+		reverse_iterator	rbegin( void );
+		reverse_iterator	rend( void );
+
+		// Const Reverse Iterators
+		const_reverse_iterator	rbegin( void ) const;
+		const_reverse_iterator	rend( void ) const;
+};
 
 #endif
