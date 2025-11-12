bb1dc2bcbf18190e341e08d62f994ec2e7a73c23
pkhienko <pannatorn1222@gmail.com>
2025-11-13 02:33:25 +0700

    Initial files (cpp, hpp, Makefile)

---
+++ RANK05/cpp09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <algorithm>
+
+# define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define BLUE "\033[1;34m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define MAGENTA "\033[1;35m"
+
+
+#endif
