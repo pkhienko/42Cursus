e7d31105607a9fbfe05b0f0e2226082d429ce527
pkhienko <pannatorn1222@gmail.com>
2025-11-16 22:54:34 +0700

    Initial files RPN.hpp

---
+++ RANK05/cpp09/ex01/include/RPN.hpp
@@ -0,0 +1,16 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <stack>
+# include <iostream>
+# include <algorithm>
+# include <exception>
+
+# define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define BLUE "\033[1;34m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define MAGENTA "\033[1;35m"
+
+#endif
