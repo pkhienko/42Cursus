b3530761dd6ee601045e8b2036be3876654e601c
pkhienko <pannatorn1222@gmail.com>
2025-11-16 23:01:31 +0700

    Initial files - main.cpp

--- RANK05/cpp09/ex01/source/main.cpp
+++ RANK05/cpp09/ex01/source/main.cpp
@@ -1,22 +1,6 @@
-#include "BitcoinExchange.hpp"
+#include "RPN.hpp"
 
 int	main( int ac, char **av )
 {
-	if (ac != 2)
-	{
-		std::cerr << "Usage: ./btc [Input File]"  << std::endl;
-		return (1);
-	}
-	
-	try
-	{
-		BitcoinExchange	btc;
-		btc.run(av[1]);
-	}
-	catch(const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	
 	return (0);
 }
