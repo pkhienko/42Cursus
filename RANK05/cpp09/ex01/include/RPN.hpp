1aaecb8c33407223bec315534e53cb866d8fab6b
pkhienko <pannatorn1222@gmail.com>
2025-11-17 19:17:05 +0700

    Implement RPN.hpp

--- RANK05/cpp09/ex01/include/RPN.hpp
+++ RANK05/cpp09/ex01/include/RPN.hpp
@@ -2,9 +2,10 @@
 # define RPN_HPP
 
 # include <stack>
+# include <sstream>
 # include <iostream>
 # include <algorithm>
-# include <exception>
+# include <stdexcept>
 
 # define RESET "\033[0m"
 # define RED "\033[1;31m"
@@ -12,5 +13,28 @@
 # define GREEN "\033[1;32m"
 # define YELLOW "\033[1;33m"
 # define MAGENTA "\033[1;35m"
+
+class RPN {
+	private :
+		std::stack<int>	_stack;
+
+		// Methods
+		bool	isOperator( const char &op ) const;
+		int		calculate( int a, int b, char op) const;
+
+	public :
+		// Constructors
+		RPN( void );
+		RPN( const RPN &other );
+
+		// Operators Overload
+		RPN	&operator=( const RPN &other );
+
+		// Destructor
+		~RPN( void );
+
+		// Method
+		void	execute( const std::string &expression );
+};
 
 #endif
