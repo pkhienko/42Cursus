6b1c729d0926ddc4da85eccb483f918ff3d03ddb
pkhienko <pannatorn1222@gmail.com>
2025-11-17 19:17:19 +0700

    Implement main.cpp

--- RANK05/cpp09/ex01/source/main.cpp
+++ RANK05/cpp09/ex01/source/main.cpp
@@ -2,5 +2,21 @@
 
 int	main( int ac, char **av )
 {
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./RPN [Input File]"  << std::endl;
+		return (1);
+	}
+	
+	try
+	{
+		RPN	rpn;
+		rpn.execute(av[1]);
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << RED << "Error" RESET ": " << e.what() << std::endl;
+	}
+	
 	return (0);
 }
