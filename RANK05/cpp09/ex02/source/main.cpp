42a96309d07dee240b6969e4e55b36c17f932b9a
pkhienko <pannatorn1222@gmail.com>
2025-11-28 19:07:09 +0700

    Implement main.cpp

--- RANK05/cpp09/ex02/source/main.cpp
+++ RANK05/cpp09/ex02/source/main.cpp
@@ -2,5 +2,15 @@
 
 int	main( int ac, char **av )
 {
+	try
+	{
+		PmergeMe	pMergeMe;
+
+		pMergeMe.execute(ac, av);
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
