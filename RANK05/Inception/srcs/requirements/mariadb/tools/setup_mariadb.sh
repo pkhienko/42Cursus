2866ba932537c53bdf6f2a3f8b4523ec316bd7b1
pkhienko <pkhienko@Inception.myguest.virtualbox.org>
2026-04-21 02:04:52 +0700

    Clean up mariadb command configuration

--- RANK05/Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ RANK05/Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -4,7 +4,7 @@ DB_PWD=$(cat /run/secrets/db_password)
 DB_ROOT_PWD=$(cat /run/secrets/db_root_password)
 
 mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld /val/lib/mysql
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
@@ -17,10 +17,11 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     done
 
     mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
-    mariadb -uroot -p"${DB_ROOT_PWD}" -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-    mariadb -uroot -p"${DB_ROOT_PWD}" -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';"
-    mariadb -uroot -p"${DB_ROOT_PWD}" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';"
-    mariadb -uroot -p"${DB_ROOT_PWD}" -e "FLUSH PRIVILEGES;"
+    mariadb -uroot -p"${DB_ROOT_PWD}" -e "\
+    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';
+    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;"
 
     mariadb-admin -uroot -p"${DB_ROOT_PWD}" shutdown
     wait "$pid"
