556ec730016ed77a950d49b5646016be7c663b32
pkhienko <pannatorn1222@gmail.com>
2026-05-15 11:47:33 +0700

    add more test

--- RANK05/cpp07/ex00/source/main.cpp
+++ RANK05/cpp07/ex00/source/main.cpp
@@ -2,6 +2,7 @@
 
 int	main( void )
 {
+	// ---- Subject Test ----
 	int a = 2;
 	int b = 3;
 
@@ -17,5 +18,14 @@ int	main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	return (0);
+
+
+	// ---- Additional Test ----
+	int e = 5;
+	int f = 5;
+
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+ 	return (0);
 }
